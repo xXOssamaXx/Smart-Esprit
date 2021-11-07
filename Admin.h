431375f8ccf0f2ed07a6d0b156e9d709882d95e2
oussama <Oussama.saihi@esprit.tn>
2021-11-07 16:00:58 +0100

    "this is a header file"

---
+++ Admin.h
@@ -0,0 +1,12 @@
+//Un Administrateur peut ajouter , modifier , supprimer ... les utlisateur .
+#include<stdio.h>
+typedef struct utilisateur//il faut definer une structure des utilisateurs.
+int ajouter (utilisateur );//
+int modifier();
+int supprimer();
+int chercher ();
+int afiicher();
+int nombre_etudiant();
+//uner fonction calendrier peut slectioner un jour pour consulter les menus prevus pour un jour .
+typedef struct date  // definir une struct date .
+int calendrier () ;
